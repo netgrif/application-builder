79570cf21e8859cc6f0122cad93125d950025f28
Milan Mladoniczky <6153201+tuplle@users.noreply.github.com>
2023-12-11 19:26:08 +0100

    Add env variables to configure links in builder

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,13 @@
 FROM node:18 as build
+MAINTAINER NETGRIF <devops@netgrif.com>
 WORKDIR /app
 COPY . .
 RUN npm install --legacy-peer-deps
 RUN npm run build
 
 FROM nginx:alpine
-MAINTAINER Netgrif <devops@netgrif.com>
+MAINTAINER NETGRIF <devops@netgrif.com>
 COPY default.nginx.conf /etc/nginx/conf.d/default.conf
 COPY --from=build /app/dist/application-builder/ /usr/share/nginx/html/
 EXPOSE 80
-ENTRYPOINT ["nginx","-g","daemon off;"]
+ENTRYPOINT ["/bin/sh","-c","envsubst < /usr/share/nginx/html/env.template.js > /usr/share/nginx/html/env.js && exec nginx -g 'daemon off;'"]
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,88 +1,93 @@
 import {AfterViewInit, Component, HostListener} from '@angular/core';
-import {LanguageService} from '@netgrif/components-core';
 import {MatDialog} from '@angular/material/dialog';
-import {DialogConfirmComponent} from './dialogs/dialog-confirm/dialog-confirm.component';
-import {JoyrideService} from 'ngx-joyride';
-import {TutorialService} from './tutorial/tutorial-service';
-import {MortgageService} from './modeler/mortgage.service';
 import {Router} from '@angular/router';
-import {ModelService} from './modeler/services/model/model.service';
-import {ModelerConfig} from './modeler/modeler-config';
+import {LanguageService} from '@netgrif/components-core';
+import {NetgrifApplicationEngine} from '@netgrif/components-core/';
+import {JoyrideService} from 'ngx-joyride';
+import {AppBuilderConfigurationService} from './app-builder-configuration.service';
+import {DialogConfirmComponent} from './dialogs/dialog-confirm/dialog-confirm.component';
 import {
-    DialogLocalStorageModelComponent
+  DialogLocalStorageModelComponent,
 } from './dialogs/dialog-local-storage-model/dialog-local-storage-model.component';
 import {ModelImportService} from './modeler/model-import-service';
+import {ModelerConfig} from './modeler/modeler-config';
+import {MortgageService} from './modeler/mortgage.service';
+import {ModelService} from './modeler/services/model/model.service';
+import {TutorialService} from './tutorial/tutorial-service';
 
 @Component({
-    selector: 'nab-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss']
+  selector: 'nab-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements AfterViewInit {
-    title = 'Netgrif Application Builder';
+  title = 'Netgrif Application Builder';
+  config: NetgrifApplicationEngine;
 
-    @HostListener('window:beforeunload', ['$event'])
-    WindowBeforeUnload($event: any) {
-        $event.returnValue = 'Your data will be lost!';
+  @HostListener('window:beforeunload', ['$event'])
+  WindowBeforeUnload($event: any) {
+    $event.returnValue = 'Your data will be lost!';
+  }
+
+  constructor(
+    _config: AppBuilderConfigurationService,
+    private router: Router,
+    private _languageService: LanguageService,
+    private matDialog: MatDialog,
+    private readonly joyrideService: JoyrideService,
+    private _mortgageService: MortgageService,
+    private tutorialService: TutorialService,
+    private modelService: ModelService,
+    private importService: ModelImportService,
+  ) {
+    this.config = _config.get();
+  }
+
+  ngAfterViewInit(): void {
+    // TODO: NAB-326 https://developer.mozilla.org/en-US/docs/Web/API/Broadcast_Channel_API
+    const oldModel = localStorage.getItem(ModelerConfig.LOCALSTORAGE.DRAFT_MODEL.KEY);
+    if (!oldModel) {
+      return;
     }
+    const dialogRef = this.matDialog.open(DialogLocalStorageModelComponent, {
+      data: {
+        id: localStorage.getItem(ModelerConfig.LOCALSTORAGE.DRAFT_MODEL.ID),
+        timestamp: localStorage.getItem(ModelerConfig.LOCALSTORAGE.DRAFT_MODEL.TIMESTAMP),
+        title: localStorage.getItem(ModelerConfig.LOCALSTORAGE.DRAFT_MODEL.TITLE),
+      },
+    });
+    dialogRef.afterClosed().subscribe(result => {
+      if (result === true) {
+        this.importService.importFromXml(oldModel);
+      } else if (result === false) {
+        localStorage.clear();
+      }
+    });
+  }
 
-    constructor(
-        private router: Router,
-        private _languageService: LanguageService,
-        private matDialog: MatDialog,
-        private readonly joyrideService: JoyrideService,
-        private _mortgageService: MortgageService,
-        private tutorialService: TutorialService,
-        private modelService: ModelService,
-        private importService: ModelImportService
-    ) {
-    }
+  addMortgage() {
+    const dialogRef = this.matDialog.open(DialogConfirmComponent);
 
-    ngAfterViewInit(): void {
-        // TODO: NAB-326 https://developer.mozilla.org/en-US/docs/Web/API/Broadcast_Channel_API
-        const oldModel = localStorage.getItem(ModelerConfig.LOCALSTORAGE.DRAFT_MODEL.KEY);
-        if (!oldModel) {
-            return;
-        }
-        const dialogRef = this.matDialog.open(DialogLocalStorageModelComponent, {
-            data: {
-                id: localStorage.getItem(ModelerConfig.LOCALSTORAGE.DRAFT_MODEL.ID),
-                timestamp: localStorage.getItem(ModelerConfig.LOCALSTORAGE.DRAFT_MODEL.TIMESTAMP),
-                title: localStorage.getItem(ModelerConfig.LOCALSTORAGE.DRAFT_MODEL.TITLE),
-            }
-        });
-        dialogRef.afterClosed().subscribe(result => {
-            if (result === true) {
-                this.importService.importFromXml(oldModel);
-            } else if (result === false) {
-                localStorage.clear();
-            }
-        });
-    }
+    dialogRef.afterClosed().subscribe(result => {
+      if (result === true) {
+        this._mortgageService.loadModel();
+        this.router.navigate(['/modeler']);
+      }
+    });
+  }
 
-    addMortgage() {
-        const dialogRef = this.matDialog.open(DialogConfirmComponent);
+  help() {
+    this.joyrideService.startTour({
+      steps: this.tutorialService.steps,
+      themeColor: '#0f4c81dd',
+    });
+  }
 
-        dialogRef.afterClosed().subscribe(result => {
-            if (result === true) {
-                this._mortgageService.loadModel();
-                this.router.navigate(['/modeler']);
-            }
-        });
-    }
+  get tutorial() {
+    return this.tutorialService;
+  }
 
-    help() {
-        this.joyrideService.startTour({
-            steps: this.tutorialService.steps,
-            themeColor: '#0f4c81dd'
-        });
-    }
-
-    get tutorial() {
-        return this.tutorialService;
-    }
-
-    openInTab(url: string) {
-        window.open(url, '_blank');
-    }
+  openInTab(url: string) {
+    window.open(url, '_blank');
+  }
 }
